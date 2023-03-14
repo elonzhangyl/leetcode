028fde8386ab6eecb92bfd4367a187313675bd74
ElonZhang <39813443+elonzhangyl@users.noreply.github.com>
2023-03-14 11:23:37 +0000

    Update 19.py

--- 8. backtracing/19.py
+++ 8. backtracing/19.py
@@ -1,29 +1,30 @@
-
+# [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+# ["JFK", "MUC", "LHR", "SFO", "SJC"]
+from collections import defaultdict
 class Solution:
     def __init__(self):
-        self.res = []
         self.path = []
-        self.record = []
+        self.res = []
 
-    def subset(self, nums):
-        self.record = [False] * len(nums)
-        self.backtracking(nums)
+    def trip(self, tickets):
+        tickets_dict = defaultdict(list)
+        for item in tickets:
+            tickets_dict[item[0]].append(item[1])
+        for airport in tickets_dict:
+            tickets_dict[airport].sort()
+        self.backtracking(tickets, 'JFK')
         return self.res
 
-    def backtracking(self, nums):
-        if len(self.path) == len(nums):
+    def backtracking(self, tickets, start):
+        if len(tickets) == 0:
             self.res.append(self.path[:])
-            return
+            return True
 
-        for i in range(len(nums)):
-            if self.path and nums[i][0] != self.path[-1][1]:
-                continue
-            if self.record[i] == True:
-                continue
-            self.path.append(nums[i])
-            self.record[i] = True
-            self.backtracking(nums)
-            self.record[i] = False
-            self.path.pop()
+        for i in range(len(tickets)):
+            if tickets[i][0] == start:
+                self.path.append(tickets[i])
+                if self.backtracking(tickets[0:i] + tickets[i + 1:], tickets[i][1]):
+                    return True
+                self.path.pop()
 
-print(Solution().subset([["1", "2"], ["1", "3"], ["3", "1"]]))
+print(Solution().trip([["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]))
