9fcadfc9f8cfa58bea0a9a6b56fa2261dc2ddfab
Yulong Zhang <elon.zhangyl@outlook.com>
2023-03-15 21:51:53 +0000

    update
    up

--- 8. backtracing/20.py
+++ 8. backtracing/20.py
@@ -1,38 +1,53 @@
-
+from copy import deepcopy
+import numpy as np
 class Solution:
     def __init__(self):
         self.res = []
-        self.path = []
-        self.record = []
-        self.layer = -1
-        self.record_column = []
+        self.col = []
+        self.diag = []
+        
+    def queens(self, n):
+        chess = [['.'] * n for _ in range(n)]
+        self.backtracking(n, 0, chess)
+        res_fit = [[0] * n for _ in range(len(self.res))]
+        for i, res in enumerate(self.res):
+            for j, row in enumerate(res):
+                res_fit[i][j] = ''.join(row)
+        return res_fit
 
-    def subset(self, n):
-        self.record = [[False] * n for _ in range(n)]
-        self.record_column = [False] * n
-        self.backtracking(n)
-        return self.res
-
-    def backtracking(self, n):
-        if len(self.path) == n:
-            self.res.append(self.path[:])
+    def backtracking(self, n, row, board):
+        if row == n:
+            self.res.append(deepcopy(board))
             return
 
-        for i in range(n):
-            if self.record_column[i] == True:
+        for col in range(n):
+            if not self.is_valid(board, row, col):
                 continue
-            if i >= 1 and self.record[self.layer][i-1] == True:
-                continue
-            if i <= n - 2 and self.record[self.layer][i+1] == True:
-                continue
-            self.path.append(i)
-            self.layer += 1
-            self.record_column[i] = True
-            self.record[self.layer][i] = True
-            self.backtracking(n)
-            self.record[self.layer][i] = False
-            self.record_column[i] = False
-            self.layer -= 1
-            self.path.pop()
+            board[row][col] = 'Q'
+            self.backtracking(n, row + 1, board)
+            board[row][col] = '.'
 
-print(Solution().subset(1))
+    def is_valid(self, board, row, col):
+        for i in range(len(board)):
+            if board[i][col] == 'Q':
+                return False
+        
+        up = row
+        left = col
+        while up >= 0 and left >= 0:
+            if board[up][left] == 'Q':
+                return False
+            up -= 1
+            left -= 1
+
+        up2 = row
+        right = col
+        while up2 >= 0 and right < len(board):
+            if board[up2][right] == 'Q':
+                return False
+            up2 -= 1
+            right += 1
+
+        return True
+
+print(Solution().queens(4))
---
+++ 8. backtracing/21-1.py
@@ -0,0 +1,44 @@
+class Solution:
+    def __init__(self) -> None:
+        self.blanks = []
+        self.board = None
+
+    def shudu(self, board):
+        self.board = board
+        for i in range(9):
+            for j in range(9):
+                if self.board[i][j] == '.':
+                    self.blanks.append([i, j])
+        self.backtracking(0, len(self.blanks))
+        return self.board
+
+    def is_valid(self, row, col, val):
+        if val in self.board[row]:
+            return False
+        for r in range(9):
+            if val == self.board[r][col]:
+                return False
+        for i in range(row // 3 * 3, row // 3 * 3 + 3):
+            for j in range(col // 3 * 3, col // 3 * 3 + 3):
+                if self.board[i][j] == val:
+                    return False
+        return True
+
+    def backtracking(self, idx, len_blank):
+        print(f'idx={idx}, len_blank={len_blank}')
+        if idx == len_blank:
+            # self.board = deepcopy(self.board)
+            return True
+        
+        for m in range(1, 10):
+            print(f'm={m}')
+            if not self.is_valid(self.blanks[idx][0], self.blanks[idx][1], str(m)):
+                continue
+            self.board[self.blanks[idx][0]][self.blanks[idx][1]] = str(m)
+            if self.backtracking(idx + 1, len_blank):
+                return True
+            self.board[self.blanks[idx][0]][self.blanks[idx][1]] = '.'
+
+board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+
+print(Solution().shudu(board))
